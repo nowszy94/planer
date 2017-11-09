4e2a0353ddc9f2fb021448aba2e8a07096346ce4
Szymon Nowak <szymon.nowak.sn1@contractors.roche.com>
2017-11-09 18:08:09 +0100

    initial commit

---
+++ src/main/java/com/sda/planer/planer/PlanerApplication.java
@@ -0,0 +1,12 @@
+package com.sda.planer.planer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanerApplication.class, args);
+	}
+}
